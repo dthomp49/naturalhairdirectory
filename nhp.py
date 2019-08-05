fe517f5418b995317a48673daabcdf1c9c1cbfc8
dthomp49 <53706813+dthomp49@users.noreply.github.com>
2019-08-04 21:00:28 -0400

    Create nhp.py

---
+++ nhp.py
@@ -0,0 +1,24 @@
+print ("Welcome to the Natural Hair Directory!")
+
+name = input("What's your name?")
+print ("Hello! Nice to meet you,", name)
+
+print ("The purpose of this program is to help you determine the best products for your hair type.")
+print ("Natural hair is extremely diverse, so not everyone will have the same needs.  Let's get started!")
+
+hair = input("Now what is your curl type: 2, 3, or 4?")
+hair = int(hair)
+if hair == 3:
+    print ("You might want to try a light moisture milk by Shea Moisture.")
+elif hair > 3:
+    print ("You have a tighter curl pattern.  Try some whipped shea butter.")
+elif hair < 3:
+    print ("Your hair is probably wavy.  Lighter oils would be best for you.")
+
+porosity = input("What is your porosity?")
+if porosity == "high":
+    print ("Try protein to strengthen your strands.")
+elif porosity == "low":
+    print ("Low porosity needs heat to absorb moisture.")
+elif porosity == "medium":
+    print ("You won the hair lottery! Alternate between heat and protein regularly.")
